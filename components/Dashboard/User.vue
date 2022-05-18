a1269579d80af9b5ef02018e6b7e1af10ecfeae2
igorkrosh <igor.krosh@gmail.com>
2022-05-18 15:38:01 +0300

    Блок рекомендаций в мобильной версии теперь в самом низу

--- components/Dashboard/User.vue
+++ components/Dashboard/User.vue
@@ -1,22 +1,28 @@
 <template lang="pug">
-    .col-wrapper
-        .col.first 
-            .progress-wrapper.block(v-if="this.$store.getters.PROGRESS.length > 0")
-                h2 Прогресс:
-                ProfileProgressItem(v-for="(item, index) in this.$store.getters.PROGRESS" :key="index" :item="item")
-            .webinar.block(v-if="this.$store.getters.WEBINAR.length > 0")
-                h2 Предстоящие вебинары:
-                .webinar-wrapper
-                    ProfileWebinarItem(v-for="(item, index) in this.$store.getters.WEBINAR" :key="index" :item="item")
-            .results.block(v-if="this.$store.getters.RESULTS.length > 0")
-                h2 Результаты:
-                .results-wrapper
-                    ProfileResultItem(v-for="(item, index) in this.$store.getters.RESULTS" :key="index" :item="item")
-            .recomendation.block(v-if="this.$store.getters.RECOMENDATIONS.length > 0")
-                h2 Рекомендации:
-                .wrapper
-                    CourseCard(v-for="(item, index) in this.$store.getters.RECOMENDATIONS" :key="index" :item="item")
+.col-wrapper
+    .col.first 
+        .progress-wrapper.block(v-if="this.$store.getters.PROGRESS.length > 0")
+            h2 Прогресс:
+            ProfileProgressItem(v-for="(item, index) in this.$store.getters.PROGRESS" :key="index" :item="item")
+        .webinar.block(v-if="this.$store.getters.WEBINAR.length > 0")
+            h2 Предстоящие вебинары:
+            .webinar-wrapper
+                ProfileWebinarItem(v-for="(item, index) in this.$store.getters.WEBINAR" :key="index" :item="item")
+        .results.block(v-if="this.$store.getters.RESULTS.length > 0")
+            h2 Результаты:
+            .results-wrapper
+                ProfileResultItem(v-for="(item, index) in this.$store.getters.RESULTS" :key="index" :item="item")
+        .recomendation.block(v-if="this.$store.getters.RECOMENDATIONS.length > 0")
+            h2 Рекомендации:
+            .wrapper
+                CourseCard(v-for="(item, index) in this.$store.getters.RECOMENDATIONS" :key="index" :item="item")
+    .col.second
         ProfileColumnInfo
+        .recomendation.block(v-if="this.$store.getters.RECOMENDATIONS.length > 0")
+            h2 Рекомендации:
+            .wrapper
+                CourseCard(v-for="(item, index) in this.$store.getters.RECOMENDATIONS" :key="index" :item="item")
+
 </template>
 
 <script>
--- components/Profile/ColumnInfo.vue
+++ components/Profile/ColumnInfo.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-    .col.second
-        CalendarBlock(v-if="calendar")
-        .journal.block(v-if="this.$store.getters.JOURNAL.length > 0")
-            h2 Задания:
-            .item-wrapper
-                ProfileJournalItem(v-for="(item, index) in this.$store.getters.JOURNAL" :key="index" :item="item")
-                button.btn(v-if="this.$store.getters.USER.role != 'user'") Добавить
-        .achievements.block(v-if="this.$store.getters.USER.role == 'user'")
-            h2 Достижения:
-            .item-wrapper 
-                ProfileAchievementsItem(v-for="(item, index) in this.$store.getters.ACHIEVEMENTS" :key="index" :item="item")
+.col.second
+    CalendarBlock(v-if="calendar")
+    .journal.block(v-if="this.$store.getters.JOURNAL.length > 0")
+        h2 Задания:
+        .item-wrapper
+            ProfileJournalItem(v-for="(item, index) in this.$store.getters.JOURNAL" :key="index" :item="item")
+            button.btn(v-if="this.$store.getters.USER.role != 'user'") Добавить
+    .achievements.block(v-if="this.$store.getters.USER.role == 'user'")
+        h2 Достижения:
+        .item-wrapper 
+            ProfileAchievementsItem(v-for="(item, index) in this.$store.getters.ACHIEVEMENTS" :key="index" :item="item")
 </template>
 
 <script>
