5326e905456c066afc1b36ba7911fef185d2d4f1
igorkrosh <igor.krosh@gmail.com>
2022-05-30 17:58:01 +0300

    FIX: Проверка авторизации на роутах

--- plugins/route.js
+++ plugins/route.js
@@ -1,9 +1,30 @@
 export default function ({ app }) {
-    /*
+    
     app.router.beforeEach((to, from, next) => {
-        app.store.dispatch('SET_LOADING', true)
-        console.log('route')
-        setTimeout(next, 500);
+        console.log(to)
+
+        if (to.name == 'index')
+        {
+            app.router.push('/login');
+        }
+
+        console.log()
+
+        app.$axios.get('/api/user')
+        .then(response => {
+            next();
+        })
+        .catch(error => {
+            if (to.name == 'login' || to.name == 'register')
+            {
+                next();
+            }
+            else 
+            {
+                app.router.push('/login');
+                next();
+            }
+        })
     })
-    */
+    
 }
