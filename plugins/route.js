32e53ae7115273cd7d4ee1333ff851c7f57fbd84
igorkrosh <igor.krosh@gmail.com>
2022-07-25 22:37:24 +0300

    FIX: Редирект при ините на логин

--- plugins/route.js
+++ plugins/route.js
@@ -1,33 +1,5 @@
-export default function ({ app }) {
-    /*
-    app.router.beforeEach((to, from, next) => {
-        app.$axios.get('/api/user')
-        .then(response => {
-            if (to.name == 'login' || to.name == 'register')
-            {
-                app.router.push('/dashboard');
-                next();
-            }
-            next();
-        })
-        .catch(error => {
-            console.log(error)
-            if (to.name == 'login' || to.name == 'register')
-            {
-                next();
-            }
-            else 
-            {
-                app.router.push('/login');
-                next();
-            }
-        })
-    })
-    */
-
-    console.log('route')
-
-    app.$axios.get('/api/user')
+export default async function ({ app }) {
+    await app.$axios.get('/api/user')
     .then(response => {
         app.store.dispatch('SET_AUTH', true);
         console.log(response)
